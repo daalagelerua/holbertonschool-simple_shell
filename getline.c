82421fecd50e281dfa148309a42b85931b464904
daalagelerua <aurele284@gmail.com>
2025-01-06 11:03:51 +0100

    modification de getline pour ajouter l'execution de la commande

--- getline.c
+++ getline.c
@@ -10,8 +10,11 @@
 int main(void)
 {
 char *line = NULL; /*pointeur pour stocker l'entrée utilisateur*/
+char *argv[];
 size_t len = 0; /*taille de la mémoire alloué à line*/
 ssize_t nread; /*nombre de caractère lus*/
+pid_t pid;
+int status;
 
 while (1) /*boucle infinie pour garder le shell actif*/
 	{
@@ -24,7 +27,29 @@ while (1) /*boucle infinie pour garder le shell actif*/
 		break;
 		}
 
-	printf("Vous avez tapé : %s, line); /*affiche la commande saisie*/
+	line[nread - 1] = '\0'; /*supprime le \n en fin de commande*/
+
+	if (strcmp(line, "exit") == 0)
+		break;
+
+	pid = fork(); /*crée un process enfant*/
+	if (pid == -1)
+		{
+		perror("erreur fork");
+		continue;
+		}
+
+	if (pid == 0) /*process enfant*/
+		{
+		*argv[] = {line, NULL}; /*argument à passer à execve*/
+		if (execve(line, argv, NULL) == -1)
+			{
+			perror(line); /*affiche une erreur si execve echoue*/
+			exit(EXIT_FAILURE); /*quitte le process enfant*/
+			}
+		}
+	else /*process parent*/
+		wait(&status); /*attend que l'enfant se termine*/
 	}
 free(line); /*libère la mémoire*/
 return(0);
