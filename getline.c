72b2905c40d9626bd82b2dbd6a7867f11ad08a41
daalagelerua <aurele284@gmail.com>
2025-01-06 14:39:18 +0100

    reorganisation de la fonction pour ne pas depasser 40 lignes et ajout de la fonction access

--- getline.c
+++ getline.c
@@ -5,6 +5,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/*protos*/
+void display_prompt(void);
+void execute_command(char *line, char **argv);
+
 /**
 * main - boucle principale et affichage du prompt
 * @argc: compteur d'argument (non utilisé ici)
@@ -15,17 +19,14 @@
 int main(int argc, char **argv)
 {
 char *line = NULL; /*pointeur pour stocker l'entrée utilisateur*/
-char *cmd_argv[2];
 size_t len = 0; /*taille de la mémoire alloué à line*/
 ssize_t nread; /*nombre de caractère lus*/
-pid_t pid;
-int status;
 
 (void)argc; /*argc n'est pas utilisé donc on le mute*/
 
 while (1) /*boucle infinie pour garder le shell actif*/
 	{
-	printf("($) "); /*affiche le prompt*/
+	display_prompt(); /*affiche le prompt*/
 	nread = getline(&line, &len, stdin); /*lit l'entrée utilisateur*/
 
 	if (nread == -1) /*verifie si EOF ou erreur*/
@@ -39,28 +40,48 @@ while (1) /*boucle infinie pour garder le shell actif*/
 	if (strcmp(line, "exit") == 0)
 		break;
 
-	pid = fork(); /*crée un process enfant*/
-	if (pid == -1)
-		{
-		perror("erreur fork");
-		continue;
-		}
-
-	if (pid == 0) /*process enfant*/
-		{
-		cmd_argv[0] = line; /*commande a executer*/
-		cmd_argv[1] = NULL; /*fin de l'argument*/
-
-		if (execve(line, cmd_argv, NULL) == -1)
-			{
-			/*affiche erreur avec nom du prog*/
-			fprintf(stderr, "%s: 1: %s: not found\n", argv[0], line);
-			exit(EXIT_FAILURE); /*quitte le process enfant*/
-			}
-		}
-	else /*process parent*/
-		wait(&status); /*attend que l'enfant se termine*/
+	execute_command(line, argv); /*execute la commande*/
 	}
 free(line); /*libère la mémoire*/
 return(0);
+}
+
+/**
+* display_prompt - affiche le shell prompt
+*/
+
+void display_prompt(void)
+{
+printf("cisfun$ ");
+fflush(stdout); /*assure que le prompt est affiché immediatement*/
+}
+
+/**
+* execute_command - crée les forks et execute les commandes
+* @line: entrée utilisateur
+* @argv: vecteur d'argument de la fonction main
+*/
+
+void execute_command(char *line, char **argv)
+{
+pid_t pid;
+int status;
+
+pid = fork();
+if (pid == -1)
+	{
+	perror("erreur fork");
+	return;
+	}
+
+if (pid == 0) /*process enfant*/
+	{
+	if (access(line, X_OK) ==1) /*verifie si la commande est executable*/
+		{
+		fprintf(stderr, "%s: %s: permission or cmd not found\n", argv[0], line);
+		exit(EXIT_FAILURE);
+		}
+	}
+	else /*process parent*/
+		wait(&status);
 }
