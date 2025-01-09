6e4b4549d84e98e058bbe3c6198c85dcedca6cc3
daalagelerua <aurele284@gmail.com>
2025-01-09 13:56:28 +0100

    changement allocation memoire buf_siz

--- path.c
+++ path.c
@@ -12,7 +12,7 @@ char *path, *dir, *full_path;
 size_t command_len, buf_siz;
 path = getenv("PATH");
 
-if (!path) /*si path n'existe pas ou est NULL*/
+if (!path || *path == '\0') /*si path n'existe pas ou est NULL*/
 	return (NULL);
 command_len = strlen(command); /*stocke la longueur du nom de la cmd*/
 path = strdup(path); /*duplique path car strtok va modifier la chaine*/
@@ -23,7 +23,7 @@ while (dir) /*parcourt chaque repertoire*/
 	{ /*longueur repertoire + longueur commande*/
 		/* +2 pour '/' entre rep et cmd et '\0'*/
 	buf_siz = strlen(dir) + command_len + 2;
-	full_path = malloc(strlen(dir) + command_len + 2);
+	full_path = malloc(buf_siz);
 	if (!full_path)
 		{
 		free(path);
