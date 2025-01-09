49f946e247e7e8c278099e3565673ff106ad8d14
daalagelerua <aurele284@gmail.com>
2025-01-09 13:44:10 +0100

    implementation snprintf dans path.c

--- path.c
+++ path.c
@@ -9,36 +9,41 @@
 char *find_command_path(char *command)
 {
 char *path, *dir, *full_path;
-size_t command_len;
+size_t command_len, buf_siz;
 path = getenv("PATH");
 
+if (!path) /*si path n'existe pas ou est NULL*/
+	return (NULL);
+command_len = strlen(command); /*stocke la longueur du nom de la cmd*/
+path = strdup(path); /*duplique path car strtok va modifier la chaine*/
 if (!path)
 	return (NULL);
-
-command_len = strlen(command);
-path = strdup(path);
-
-if (!path)
-	return (NULL);
-
-dir = strtok(path, ":");
-while (dir)
-	{
+dir = strtok(path, ":"); /*decoupage des repertoires*/
+while (dir) /*parcourt chaque repertoire*/
+	{ /*longueur repertoire + longueur commande*/
+		/* +2 pour '/' entre rep et cmd et '\0'*/
+	buf_siz = strlen(dir) + command_len + 2;
 	full_path = malloc(strlen(dir) + command_len + 2);
 	if (!full_path)
 		{
 		free(path);
 		return (NULL);
 		}
-	sprintf(full_path, "%s/%s", dir, command);
-	if (access(full_path, X_OK) == 0)
+	if ((snprintf(full_path, buf_siz, "%s/%s", dir, command)) >= (int)buf_siz)
+		{
+		fprintf(stderr, "error: buffer size insufficient for cmd path\n");
+		free(full_path);
+		free(path);
+		return (NULL);
+		}
+	if (access(full_path, X_OK) == 0) /*full_path existe et est executable*/
 		{
 		free(path);
 		return (full_path);
 		}
-	free(full_path);
-	dir = strtok(NULL, ":");
+	free(full_path); /*si access echoue ->free*/
+	dir = strtok(NULL, ":"); /*passe au prochain rep*/
 	}
-free(path);
+free(path); /*si aucun rep ne contient cmd ->free*/
 return (NULL);
 }
